91ebd035b90c6826921d85a9c599a1ddb5a2305c
Michal Jach <92219378+s26234@users.noreply.github.com>
2022-06-10 21:54:44 +0200

    Update cw5b.sh

--- s26234-Michal_Jach/Zajecia_2/cw5b.sh
+++ s26234-Michal_Jach/Zajecia_2/cw5b.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 A=$1
 echo "Pierwszy znak z argumentu to: "${A:0:1}
-echo "Ostatni znak z argumentu to: "${A:A-1:1}
 echo "Ostatni znak z argumentu to: "${A: -1}
 echo -e $A | sed s/SOP/`echo -e "\e[32mSOP\e[0m"`/g
